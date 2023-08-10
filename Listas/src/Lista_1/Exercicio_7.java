5ddad69fb2f5687543a1187dc284788e95d219c8
Izabela Cecilia <icsbarbosa@sga.pucminas.br>
2023-08-10 20:14:40 -0300

    exercicio 7

---
+++ Listas/src/Lista_1/Exercicio_7.java
@@ -0,0 +1,28 @@
+package Lista_1;
+import java.util.Scanner;
+
+public class Exercicio_7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número de 2 dígitos: ");
+        int numero = scanner.nextInt();
+        scanner.close();
+        int resultado = multiplicacaoPorOnze(numero);
+
+        System.out.println("Resultado: " + resultado);
+    }
+
+    public static int multiplicacaoPorOnze(int numero) {
+        int digito1 = numero / 10;
+        int digito2 = numero % 10;
+        int somaDigitos = digito1 + digito2;
+        int vaiUm = somaDigitos / 10;
+        int resultado = 0;
+
+        if(vaiUm!=0) resultado = digito1 * 100 + vaiUm * 10 + digito2;
+        resultado = digito1 * 100 + (digito1 + digito2) * 10 + digito2;
+
+        return resultado;
+    }
+}

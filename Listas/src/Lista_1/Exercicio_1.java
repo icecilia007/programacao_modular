9451bc8deb52bee9af7479a382651599dd92f809
Izabela Cecilia <icsbarbosa@sga.pucminas.br>
2023-08-10 15:15:15 -0300

    first commit

---
+++ Listas/src/Lista_1/Exercicio_1.java
@@ -0,0 +1,44 @@
+package Lista_1;
+
+import java.util.Scanner;
+
+public class Exercicio_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] vetor = new int[6];
+
+        System.out.println("Digite 6 números inteiros:");
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            vetor[i] = scanner.nextInt();
+        }
+
+        System.out.println("Vetor original:");
+        printArray(vetor);
+        invertArray(vetor);
+        System.out.println("Vetor invertido:");
+        printArray(vetor);
+
+        scanner.close();
+    }
+    public static void invertArray(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left < right) {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+    public static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}

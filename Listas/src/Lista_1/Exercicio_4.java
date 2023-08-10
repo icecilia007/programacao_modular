205da0c5dc7c840399acded0fce42a914c0c98f4
Izabela Cecilia <icsbarbosa@sga.pucminas.br>
2023-08-10 18:28:52 -0300

    exercicio 4

---
+++ Listas/src/Lista_1/Exercicio_4.java
@@ -0,0 +1,28 @@
+package Lista_1;
+import java.util.Scanner;
+public class Exercicio_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite frases para serem invertidas (digite 'fim' para encerrar):");
+
+        String frase = scanner.nextLine();
+
+        while (!frase.equalsIgnoreCase("fim")) {
+            System.out.println("Frase invertida: " + reverseString(frase));
+            frase = scanner.nextLine();
+        }
+
+        System.out.println("Programa encerrado.");
+        scanner.close();
+    }
+    public static String reverseString(String input) {
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed.append(input.charAt(i));
+        }
+
+        return reversed.toString();
+    }
+}
